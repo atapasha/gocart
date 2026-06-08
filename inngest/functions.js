9b7f888c8c11b0756ac5574709e983b476d1b423
atapasha <atapashazade@gmail.com>
2026-06-08 08:21:56 +0330

    innest changes

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,10 +1,12 @@
 import { inngest } from "./client";
 import prisma from "@/lib/prisma";
 
-// Inngest Function to save user data to a database
+// Create
 export const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-create" },
-  { event: "clerk/user.created" },
+  {
+    id: "sync-user-create",
+    event: "clerk/user.created",
+  },
   async ({ event }) => {
     const { data } = event;
 
@@ -12,38 +14,44 @@ export const syncUserCreation = inngest.createFunction(
       data: {
         id: data.id,
         email: data.email_addresses[0].email_address,
-        name: `${data.first_name} ${data.last_name}`,
+        name: `${data.first_name || ""} ${data.last_name || ""}`.trim(),
         image: data.image_url,
       },
     });
-  },
+  }
 );
 
-// Inngest Function to update user data in database
+// Update
 export const syncUserUpdation = inngest.createFunction(
-  { id: "sync-user-update" },
-  { event: "clerk/user.updated" },
+  {
+    id: "sync-user-update",
+    event: "clerk/user.updated",
+  },
   async ({ event }) => {
     const { data } = event;
+
     await prisma.user.update({
       where: { id: data.id },
       data: {
         email: data.email_addresses[0].email_address,
-        name: `${data.first_name} ${data.last_name}`,
+        name: `${data.first_name || ""} ${data.last_name || ""}`.trim(),
         image: data.image_url,
       },
     });
-  },
+  }
 );
 
-// Inngest Function to delete user from database
+// Delete
 export const syncUserDeletion = inngest.createFunction(
-  { id: "sync-user-delete" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "sync-user-delete",
+    event: "clerk/user.deleted",
+  },
   async ({ event }) => {
     const { data } = event;
+
     await prisma.user.delete({
       where: { id: data.id },
     });
-  },
+  }
 );
